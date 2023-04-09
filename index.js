8b6908d3c14e9528236b876cc8ca41d4f0b6ea85
elo1lson <eloilsonfontenele2@gmail.com>
2023-04-09 18:44:56 -0300

    feat: remove comment

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import Ifunny from "./src/ifunny.js";
 import { getToken } from "./src/ifunny.js";
-
+import { config } from "dotenv";
+config();
 const credentials = getToken().then((c) => {
-    new Ifunny({
-        email: "eloilsonfontenele2@gmail.com",
-        password: "eloilson2",
-        cookie: c.rawCookie,
-        token: c.token,
-    }).login();
-    //.post("https://i.stack.imgur.com/H186L.png", { description: "oi" });
+	new Ifunny({
+		email: process.env.EMAIL,
+		password: process.env.PASSWORD,
+		cookie: c.rawCookie,
+		token: c.token,
+	}).login();
+	//.post("https://i.stack.imgur.com/H186L.png", { description: "oi" });
 });
-
